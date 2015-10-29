20ba9e47270bb5044e9234baf50dc5ddaefba95c
Mateusz Borycki <mateuszborycki@gmail.com>
2015-10-29 14:15:22 +0100

    small changes

--- Benchmarks/main.cpp
+++ Benchmarks/main.cpp
@@ -2,17 +2,19 @@
 #include "RPN/Parser.h"
 
 
-void TestParse(const std::string &expr)
+float TestParse(const std::string &expr)
 {
-	//auto f = RPN::Parser::Default().Compile(expr);
 	auto p = RPN::Parser::Default().Parse(expr);
+	if (!p)
+		return -666.0f;
+	return p->value();
 }
 
 int main ()
 {
   using namespace std;
 
-  TestParse("2+2");
+  auto x = TestParse("2+2");
 
   cout << "Hello world" << endl;
   return 0;
